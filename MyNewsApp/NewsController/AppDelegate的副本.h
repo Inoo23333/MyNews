79f539f763d77bc79b908cc465c5c17a9a1f1604
Inoo23333 <47746986+Inoo23333@users.noreply.github.com>
2020-05-08 01:13:09 +0800

    Add files via upload

---
+++ MyNewsApp/NewsController/AppDelegate的副本.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleApp1
+//
+//  Created by user on 2020/2/4.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
